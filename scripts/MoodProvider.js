1b09e67d70f47e03cff0a1301f8f4b5b178d9c98
Caleb James <calebsjames@gmail.com>
2021-02-15 09:56:08 -0600

    updated json and added MoodProvider

---
+++ scripts/MoodProvider.js
@@ -0,0 +1,12 @@
+let moods = []
+
+export const getMoods = () => {
+    return fetch('http://localhost:8088/moods')
+        .then(response => response.json())
+        .then(parsedMoods => {
+            moods = parsedMoods
+        })
+
+}
+
+export const useMoods = () => moods.slice();
